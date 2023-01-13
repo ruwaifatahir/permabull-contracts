504282a1dc1ede5b9ed04e594a8c7d3dc3bdee70
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-01-13 07:48:30 +0500

    Add goerli support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "^0.6.12",
+  solidity: "0.6.12",
+
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_TESTNET_URL,
+      chainId: 5,
+      accounts: { mnemonic: process.env.PRIVATE_KEY },
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      goerli: process.env.GOERLI_TESTNET_API_KEY
+        ? process.env.GOERLI_TESTNET_API_KEY
+        : "",
+    },
+  },
 };
 
 export default config;
