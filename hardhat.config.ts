934da9a2316a1fa0c0c32128d410575635b11032
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-01-13 07:36:14 +0500

    add BurnKing contract source code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.6",
+  solidity: "^0.6.12",
 };
 
 export default config;
